5538b389005640010afc9f4803925f85689fbf57
MAYURESH JAGTAP <mj.mayureshjagtap@gmail.com>
2022-02-21 12:00:36 +1100

    add and delete sorted

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 const ContactCard = (props) => {
-        const {id, name, email} = props.contact;
-    return(
-        <div className="item">
-                <div className="content">
-                    <div className="header"style={{color: "black", padding: "10px"}}>{name}</div>
-                    <div style={{color: "green", padding: "10px"}}>{email}</div>
-                </div>
-                <i className="trash alternate outline icon" style={{color: "red", padding: "10px"}}></i>
-            </div>
-    );
-
+  const { id, name, email } = props.contact;
+  return (
+    <div className="item">
+      <div className="content">
+        <div className="header">{name}</div>
+        <div>{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px" }}
+        onClick={() => props.clickHander(id)}
+      ></i>
+    </div>
+  );
 };
 
 export default ContactCard;
